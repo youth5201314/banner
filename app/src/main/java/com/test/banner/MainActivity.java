1934ea5018fb63a052a82dee635a855f3f36dc96
youth5201314 <1028729086@qq.com>
2016-04-26 17:20:55 +0800

    no message

--- app/src/main/java/com/test/banner/MainActivity.java
+++ app/src/main/java/com/test/banner/MainActivity.java
@@ -2,7 +2,6 @@ package com.test.banner;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 
 import com.youth.banner.Banner;
@@ -10,19 +9,18 @@ import com.youth.banner.Banner;
 
 public class MainActivity extends Activity {
     private Banner banner;
-
+    String[] images= new String[] {
+            "http://image.zcool.com.cn/56/35/1303967876491.jpg",
+            "http://image.zcool.com.cn/59/54/m_1303967870670.jpg",
+            "http://image.zcool.com.cn/47/19/1280115949992.jpg",
+            "http://image.zcool.com.cn/59/11/m_1303967844788.jpg" };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         banner = (Banner) findViewById(R.id.banner);
-        banner.setImages(new String[] {
-         "http://img04.muzhiwan.com/2015/06/16/upload_557fd293326f5.jpg",
-         "http://img03.muzhiwan.com/2015/06/05/upload_557165f4850cf.png",
-         "http://img02.muzhiwan.com/2015/06/11/upload_557903dc0f165.jpg",
-         "http://img04.muzhiwan.com/2015/06/05/upload_5571659957d90.png",
-         "http://img03.muzhiwan.com/2015/06/16/upload_557fd2a8da7a3.jpg" });
-        banner.setOnBannerClickListener(new Banner.OnBannerClickListener() {
+        banner.setImages(images);//可以选择设置图片网址，或者资源文件
+        banner.setOnBannerClickListener(new Banner.OnBannerClickListener() {//设置点击事件
             @Override
             public void OnBannerClick(View view, int position) {
 
