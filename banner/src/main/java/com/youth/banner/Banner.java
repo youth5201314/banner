e2511f7cedc8ffa54dc26bbb25b110623da6db99
ryan <34505840@qq.com>
2019-03-13 11:34:41 +0800

    this.imageUrls = imageUrls 导致引用外部list，update的时候会把外部list清空，导致最终^C果为空

--- banner/src/main/java/com/youth/banner/Banner.java
+++ banner/src/main/java/com/youth/banner/Banner.java
@@ -233,7 +233,7 @@ public class Banner extends FrameLayout implements OnPageChangeListener {
     }
 
     public Banner setImages(List<?> imageUrls) {
-        this.imageUrls = imageUrls;
+        this.imageUrls.addAll(imageUrls);
         this.count = imageUrls.size();
         return this;
     }
