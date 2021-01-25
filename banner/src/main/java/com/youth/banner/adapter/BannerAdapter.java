38fa26ffd8deb87ebd970a875cf91d73bd004dfe
zak <linhenji@163.com>
2021-01-25 21:25:46 +0800

    feature: 优化监听注册逻辑以及减少 onPageScrolled 回调次数（去重复）

--- banner/src/main/java/com/youth/banner/adapter/BannerAdapter.java
+++ banner/src/main/java/com/youth/banner/adapter/BannerAdapter.java
@@ -5,6 +5,7 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.youth.banner.R;
 import com.youth.banner.holder.IViewHolder;
 import com.youth.banner.util.BannerUtils;
 import com.youth.banner.listener.OnBannerListener;
@@ -13,9 +14,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public abstract class BannerAdapter<T, VH extends RecyclerView.ViewHolder> extends RecyclerView.Adapter<VH> implements IViewHolder<T, VH> {
+public abstract class BannerAdapter<T, VH extends RecyclerView.ViewHolder>
+        extends RecyclerView.Adapter<VH>
+        implements IViewHolder<T, VH> {
     protected List<T> mDatas = new ArrayList<>();
-    private OnBannerListener mOnBannerListener;
+    private OnBannerListener<T> mOnBannerListener;
     private VH mViewHolder;
     private int increaseCount = 2;
 
@@ -38,15 +41,23 @@ public abstract class BannerAdapter<T, VH extends RecyclerView.ViewHolder> exten
     public final void onBindViewHolder(@NonNull VH holder, int position) {
         mViewHolder = holder;
         int real = getRealPosition(position);
+        holder.itemView.setTag(R.id.banner_data_key, mDatas.get(real));
+        holder.itemView.setTag(R.id.banner_pos_key, real);
         onBindView(holder, mDatas.get(real), real, getRealCount());
-        if (mOnBannerListener != null)
-            holder.itemView.setOnClickListener(view -> mOnBannerListener.OnBannerClick(mDatas.get(real), real));
     }
 
     @NonNull
     @Override
+    @SuppressWarnings("unchecked")
     public VH onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return onCreateHolder(parent, viewType);
+        VH vh = onCreateHolder(parent, viewType);
+        vh.itemView.setOnClickListener(v -> {
+            if (mOnBannerListener != null) {
+                mOnBannerListener.OnBannerClick((T) vh.itemView.getTag(R.id.banner_data_key),
+                        (Integer) vh.itemView.getTag(R.id.banner_pos_key));
+            }
+        });
+        return vh;
     }
 
     @Override
@@ -62,7 +73,7 @@ public abstract class BannerAdapter<T, VH extends RecyclerView.ViewHolder> exten
         return BannerUtils.getRealPosition(increaseCount == 2, position, getRealCount());
     }
 
-    public void setOnBannerListener(OnBannerListener listener) {
+    public void setOnBannerListener(OnBannerListener<T> listener) {
         this.mOnBannerListener = listener;
     }
 
