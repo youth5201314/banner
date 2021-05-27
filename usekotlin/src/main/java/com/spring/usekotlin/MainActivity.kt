39ff0ca1cc89cfdeba9f79ef0131dcf98bc7e225
wangxc <1028729086@qq.com>
2021-05-27 10:53:05 +0800

    kotlin中使用banner的demo修改
    Change-Id: I3b6d4971210fd308336f7d605a28425c9b02f67a

--- usekotlin/src/main/java/com/spring/usekotlin/MainActivity.kt
+++ usekotlin/src/main/java/com/spring/usekotlin/MainActivity.kt
@@ -7,6 +7,7 @@ import com.youth.banner.Banner
 import com.youth.banner.adapter.BannerImageAdapter
 import com.youth.banner.holder.BannerImageHolder
 import com.youth.banner.indicator.CircleIndicator
+import com.youth.banner.indicator.RoundLinesIndicator
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -21,11 +22,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var banner = (bannerLayout as Banner<String, BannerImageAdapter<String>>)
+        //使用默认的图片适配器
+        var banner = (bannerLayout1 as Banner<String, BannerImageAdapter<String>>)
         banner.apply {
             addBannerLifecycleObserver(this@MainActivity)
-            setBannerRound(20f)
-            indicator = CircleIndicator(this@MainActivity)
+            setIndicator(CircleIndicator(this@MainActivity))
             setAdapter(object : BannerImageAdapter<String>(imageUrls) {
                 override fun onBindView(holder: BannerImageHolder, data: String, position: Int, size: Int) {
                     Glide.with(this@MainActivity)
@@ -33,6 +34,16 @@ class MainActivity : AppCompatActivity() {
                             .into(holder.imageView)
                 }
             })
+        }
+
+
+        //使用自定义适配器，更多api方法自己尝试
+        var banner2 = (bannerLayout2 as Banner<String, ImageAdapter>)
+        banner2.apply {
+            addBannerLifecycleObserver(this@MainActivity)
+            setBannerRound(20f)
+            setIndicator(RoundLinesIndicator(this@MainActivity))
+            setAdapter(ImageAdapter(imageUrls))
         }
     }
 
